ed675d6744f213be1e73595ca07ab819eac89198
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-07-27 15:11:04 -0500

    Get data from request object + forms and cookies

--- loginSite.js
+++ loginSite.js
@@ -3,12 +3,16 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
+const cookieParser = require('cookie-parser')
+
 const helmet = require('helmet')
 app.use(helmet())
 
 app.use(express.static('public'))
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded()) // this will put the form name variables in req.body, collects the form data for us and parses the http msg for sent data
+
+app.use(cookieParser())
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -19,6 +23,50 @@ app.get('/', (req, res, next) => {
 
 app.get('/login', (req, res, next) => {
   res.render('login')
+})
+
+//! THE BROWSER WILL NEVER SEE THIS PAGE !//
+// grab the post login from login.ejs
+app.post('/process_login', (req, res, next) => {
+  // res.json('testing process_login')
+  //! req.body is made by urlencoded()
+  const password = req.body.password
+  const username = req.body.username
+  // check db to see if user credentials are valid w/ something like decrypt, blowfish, oAuth...etc
+
+  // if valid send to welcome page
+  // - save username in a cookie (so it's readily available)
+  //!   - cookie data is stored entirely on the browser (included w/ express)
+  //!     - and the browser will send it up to the server every time
+  //!       a request is made
+  // - could use sessions too (not included w/ express)
+  //   -  session data is stored on the server and the browser is given
+  //      essentially a key for that data
+  // - send them to welcome page
+  if (password === 'beep') {
+    // 2 args and more
+    // - a1 - name of cookie
+    // - a2 - value to set it to
+    res.cookie('username', username) // you can only set one cookie at a time
+    // 1 arg, where to send the browser
+    res.redirect('/welcome')
+  } else {
+    res.redirect('/login?msg=fail')
+  }
+
+  //   res.json(req.body)
+})
+
+app.get('/welcome', (req, res, next) => {
+  // req.cookies obj will have a property for every named cookie that has been set
+  //! but we need to require cookie-parser for this to work!
+  res.render('welcome', { username: req.cookies.username })
+})
+
+app.get('/logout', (req, res, next) => {
+  // destroy cookie
+  res.clearCookie('username')
+  res.redirect('/login')
 })
 
 app.listen(3000)
